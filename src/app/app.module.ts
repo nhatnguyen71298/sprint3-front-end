4bb2539fa478a4721b5a8da4ef8e51e7c2d1babb
Nguyen Quoc Khanh <pkhanh2994@gmail.com>
2021-01-21 16:31:23 +0700

    front end tạm thời để có cái lấy id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppTeacherModule} from './teacher/app-teacher.module';
 import {AppStudentModule} from './student/app-student.module';
 import {MaterialModule} from './material.module';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
