25be0611577ef5e5a033eb6498425ef99ebc17e7
nhatlanh2202 <nhatlanh2202@gmail.com>
2021-02-01 08:52:54 +0700

    lanh update front_end 8h50 01-02-2021

--- src/app/teacher/student-group-management/delete-group-student/delete-group-student.component.ts
+++ src/app/teacher/student-group-management/delete-group-student/delete-group-student.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {GroupStudentService} from '../../../service/group-student.service';
-import {MessageGroupStudentComponent} from "../message-group-student/message-group-student.component";
+import {MessageGroupStudentComponent} from '../message-group-student/message-group-student.component';
 
 
 @Component({
@@ -13,6 +13,7 @@ export class DeleteGroupStudentComponent implements OnInit {
   public groupStudent;
   public deleteId;
   public idMessage = 3;
+  public message = 'nothing';
 
   constructor(public dialogRef: MatDialogRef<DeleteGroupStudentComponent>,
               public groupStudentService: GroupStudentService,
@@ -27,26 +28,31 @@ export class DeleteGroupStudentComponent implements OnInit {
 
   deleteGroupStudent() {
     this.groupStudentService.deleteGroupStudent(this.deleteId).subscribe(data => {
-      if (data == null) {
-        this.dialogRef.close();
-        this.openDialogMessage();
+      this.message = data.message;
+      if (data.message === 'Complete') {
+        this.openDialogMessage('OK');
+        this.ngOnInit();
+      } else if (data.message === 'Failed') {
+        this.openDialogMessage('NotOK');
+        this.ngOnInit();
       }
+    }, () => {
+      this.message = 'Error';
+    }, () => {
+      this.ngOnInit();
     })
   }
 
-  private openDialogMessage() {
-    const timeout = 1500;
+  openDialogMessage(message): void {
     const dialogRef = this.dialog.open(MessageGroupStudentComponent, {
-      width: '500px',
-      height: '300px',
-      data: {groupMess: this.idMessage},
-      disableClose: true
+      width: '555px',
+      height: '200px',
+      data: {dataMessage: message},
+      disableClose: false
     });
-    dialogRef.afterOpened().subscribe(_ => {
-      setTimeout(() => {
-        dialogRef.close();
-      }, timeout);
-    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    })
   }
 
 }
