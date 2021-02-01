f41caf8b32f4c289b4a441c39edd65e37ad4006d
nhatprovklno3 <nguyenhongnhat71298@gmail.com>
2021-02-01 15:11:34 +0700

    merge quoc-4

--- src/app/teacher/student-group-management/delete-group-student/delete-group-student.component.ts
+++ src/app/teacher/student-group-management/delete-group-student/delete-group-student.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
-import {GroupStudentService} from '../../../service/group-student.service';
+// @ts-ignore
+import {GroupStudentService} from '../service/group-student.service';
 import {MessageGroupStudentComponent} from '../message-group-student/message-group-student.component';
 
 
